981689154789272bc6d6fd5b68dcc9def83d7a30
Nick Coblentz <nick.coblentz@gmail.com>
2023-09-20 16:50:16 -0500

    initial

---
+++ CallbacksTester/Controllers/CallbackController.cs
@@ -0,0 +1,121 @@
+﻿using CallbacksTester.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace CallbacksTester.Controllers
+{
+    [Route("api/[controller]")]        
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        private readonly ILogger<CallbackController> _logger;
+
+        public CallbackController(ILogger<CallbackController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("200OK")]        
+        public async Task<ActionResult> TwoHundredOK()
+        {
+            await LogHelper.LogRequest(Request,_logger);
+            return Ok();
+        }
+
+        [Route("VariableStatusCode")]
+        public async Task<ActionResult> VaribleStatusCode(int _code)
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            return StatusCode(_code);
+        }
+
+        [Route("Redirect302")]
+        public async Task<ActionResult> Redirect302(string location)
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            return Redirect(location);
+        }
+
+        [Route("Redirect301")]
+        public async Task<ActionResult> Redirect301(string location)
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            return RedirectPermanent(location);
+        }
+
+        [Route("Redirect308")]
+        public async Task<ActionResult> Redirect308(string location)
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            return RedirectPermanentPreserveMethod(location);
+        }
+
+        [Route("Redirect307")]
+        public async Task<ActionResult> Redirect307(string location)
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            return RedirectPreserveMethod(location);
+        }
+
+        [Route("Unauthorized401")]
+        public async Task<ActionResult> Unauthorized401()
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            return Unauthorized();
+        }
+
+        [Route("Unauthorized403")]
+        public async Task<ActionResult> Unauthorized403()
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            return StatusCode(403);
+        }
+
+        [Route("CustomResponse")]
+        public async Task<ActionResult> CustomResponse(string? contenttype="text/plain")
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            string customresponse = System.IO.File.ReadAllText("CustomResponse.txt");
+            Response.ContentType = contenttype;
+            return Content(customresponse);
+        }
+
+        [Route("CustomResponseJson")]
+        public async Task<ActionResult> CustomResponseJson()
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            string customresponse = System.IO.File.ReadAllText("CustomResponse.json");            
+            ControllerContext.HttpContext.Response.StatusCode = _code.GetValueOrDefault();
+            Response.ContentType = "application/json";
+            return Content(customresponse);
+        }
+
+        [Route("CustomResponseXML")]
+        public async Task<ActionResult> CustomResponseXML()
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            string customresponse = System.IO.File.ReadAllText("CustomResponse.xml");
+//            ControllerContext.HttpContext.Response.StatusCode = _code.GetValueOrDefault();
+            Response.ContentType = "text/xml";
+            return Content(customresponse);
+        }
+
+        [Route("CustomResponseHTML")]
+        public async Task<ActionResult> CustomResponseHTML()
+        {
+            await LogHelper.LogRequest(Request, _logger);
+            string customresponse = System.IO.File.ReadAllText("CustomResponse.html");
+            //ControllerContext.HttpContext.Response.StatusCode = _code.GetValueOrDefault();
+            Response.ContentType = "text/html";
+            return Content(customresponse);
+        }
+    }
+}
---
+++ CallbacksTester/Helpers/LogHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallbacksTester.Helpers
+{
+    public class LogHelper
+    {
+        public static async Task LogRequest(HttpRequest Request, ILogger Logger)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"\n==========\n{Request.Method} {Request.Scheme}://{Request.Host}:{Request.Path}\n");
+            if (Request.QueryString.HasValue)
+            {
+                builder.Append($"{Request.QueryString}\n");
+            }
+            foreach(var HeaderKey in Request.Headers.Keys)
+            {
+                builder.Append($"{HeaderKey}:{Request.Headers[HeaderKey]}\n");
+            }
+            if(Request.Body!=null)
+            {
+                ReadResult result = await Request.BodyReader.ReadAsync();
+                byte[] resultByteArray = result.Buffer.ToArray();
+                builder.Append($"\n{Encoding.UTF8.GetString(resultByteArray)}\n");
+            }
+            Logger.LogInformation(builder.ToString());
+        }
+    }
+}
